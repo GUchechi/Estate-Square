dad6bb16d2cc6763fcbeb21caca0de0af8ab4796
GUchechi <godswilluchendu7@gmail.com>
2023-01-24 02:13:28 +0100

    added image to firebase

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import {
   collection,
   getDocs,
@@ -13,11 +14,12 @@ import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
 import ListingItem from "./ListingItem";
 
-
-function Offers() {
+function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [lastFetchedListing, setLastFetchedListing] = useState(null);
+  //   const [lastFetchedListing, setLastFetchedListing] = useState(null)
+
+  const params = useParams();
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -28,16 +30,13 @@ function Offers() {
         // Create a query
         const q = query(
           listingsRef,
-          where("offer", "==", true),
+          where("type", "==", params.categoryName),
           orderBy("timestamp", "desc"),
           limit(10)
         );
 
         // Execute query
         const querySnap = await getDocs(q);
-
-        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-        setLastFetchedListing(lastVisible);
 
         const listings = [];
 
@@ -56,7 +55,7 @@ function Offers() {
     };
 
     fetchListings();
-  }, []);
+  }, [params.categoryName]);
 
   // Pagination / Load More
   const onFetchMoreListings = async () => {
@@ -67,17 +66,13 @@ function Offers() {
       // Create a query
       const q = query(
         listingsRef,
-        where("offer", "==", true),
+        where("type", "==", params.categoryName),
         orderBy("timestamp", "desc"),
-        startAfter(lastFetchedListing),
         limit(10)
       );
 
       // Execute query
       const querySnap = await getDocs(q);
-
-      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-      setLastFetchedListing(lastVisible);
 
       const listings = [];
 
@@ -98,7 +93,11 @@ function Offers() {
   return (
     <div className="category">
       <header>
-        <p className="pageHeader">Offers</p>
+        <p className="pageHeader">
+          {params.categoryName === "rent"
+            ? "Places for rent"
+            : "Places for sale"}
+        </p>
       </header>
 
       {loading ? (
@@ -116,20 +115,12 @@ function Offers() {
               ))}
             </ul>
           </main>
-
-          <br />
-          <br />
-          {lastFetchedListing && (
-            <p className="loadMore" onClick={onFetchMoreListings}>
-              Load More
-            </p>
-          )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>No listings for {params.categoryName}</p>
       )}
     </div>
   );
 }
 
-export default Offers;
+export default Category;
