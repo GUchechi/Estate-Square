92cbf603d64752feb16fbd5006d312f824602a86
GUchechi <godswilluchendu7@gmail.com>
2023-01-26 03:03:40 +0100

    added typewriter effect

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,40 +1,54 @@
-import { Link } from 'react-router-dom'
-import Slider from '../components/Slider'
-import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
-import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+import { Link } from "react-router-dom";
+import Slider from "../components/Slider";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+import Typewriter from "typewriter-effect";
 
 function Explore() {
   return (
-    <div className='explore'>
-      <header>
-        <p className='pageHeader'>Explore</p>
+    <div className="explore">
+      <header className="pageHeader">
+        {" "}
+        <Typewriter
+          options={{
+            autoStart: true,
+            loop: true,
+            delay: 40,
+            strings: [
+              "Explore Estate Square",
+              "You Invest, We do the Rest",
+              "Living Spaces Designed With Excellence",
+              "Smart Homes in a Smart Estate",
+            ],
+          }}
+        />
       </header>
 
       <main>
         <Slider />
 
-        <p className='exploreCategoryHeading'>Categories</p>
-        <div className='exploreCategories'>
-          <Link to='/category/rent'>
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
             <img
               src={rentCategoryImage}
-              alt='rent'
-              className='exploreCategoryImg'
+              alt="rent"
+              className="exploreCategoryImg"
             />
-            <p className='exploreCategoryName'>Places for rent</p>
+            <p className="exploreCategoryName">Places for rent</p>
           </Link>
-          <Link to='/category/sale'>
+          <Link to="/category/sale">
             <img
               src={sellCategoryImage}
-              alt='sell'
-              className='exploreCategoryImg'
+              alt="sell"
+              className="exploreCategoryImg"
             />
-            <p className='exploreCategoryName'>Places for sale</p>
+            <p className="exploreCategoryName">Places for sale</p>
           </Link>
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default Explore
+export default Explore;
